f4a607035348caf68d8530ce61f63822cdf01738
Ankit Gour <ankitgour19168893@gmail.com>
2023-08-13 14:03:20 +0530

    initial commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+   <>
+
+
+<div class="fixed bottom-0 left-0 z-50 w-full bg-gray-400">
+    <div class="flex justify-between my-2 ">
+      <img src='backward.png' className='w-7 px-1 cursor-pointer'/>
+      <img src='dark.png' className='w-7 px-1 cursor-pointer'/>
+      <img src='home.png' className='w-7 px-1 cursor-pointer'/>
+      <img src='bell.png' className='w-7 px-1 cursor-pointer'/>
+      <img src='forward.png' className='w-7 px-1 cursor-pointer'/>
+    </div>
+</div>
+
+  
+   </>
+  )
+}
